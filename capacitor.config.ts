ac1bd081d686e03a40adf41b74f01e60d2a1f751
vaaalfranca <vaaalfranca@gmail.com>
2026-07-19 15:30:49 -0300

    Estrutura do projeto inicializada e configurada com .gitignore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cortaproingles.shadowing',
+  appName: 'ShadowingApp',
+  webDir: 'dist'
+};
+
+export default config;
